2631303ce02bd49c1e95bbf43e402adaba91f00d
andre1 <andre1@AndreLaptop1.Home>
2015-04-19 13:50:43 +1000

    asdasdasd

--- Perl/Assignment1/partA.pl
+++ Perl/Assignment1/partA.pl
@@ -4,8 +4,11 @@ use warnings;
 use strict;
 use FileHandle;
 
-
-my $file1 = "original.txt`;
-print $file1;
+my $filepath1 = 'C:\Users\andre1\git\perlTutorials\Perl\Assignment1\original.txt';
+if (-T $filepath1){
+	print "exists";
+}else{
+	print "does not exist";
+}
 
 exit;
