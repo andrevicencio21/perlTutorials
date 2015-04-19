d344faa4e7542182fbee7d0e3b21d7b8b2d1655c
andre1 <andre1@AndreLaptop1.Home>
2015-04-19 19:02:09 +1000

    asdasd

--- Perl/Assignment1/partA.pl
+++ Perl/Assignment1/partA.pl
@@ -3,29 +3,42 @@ use 5.010;
 use warnings;
 use strict;
 
+print "-------------File Compare Script-------------\n";
+print
+"*Please make sure files to compare are in the same Directory as the script\n";
+print "*Enter File Name for the first file:\n";
+my $firstFilePath = <STDIN>;
+chomp($firstFilePath);
+checkTextFile($firstFilePath);
+print "Enter File Name for the second file:\n";
+my $secondFilePath = <STDIN>;
+chomp($secondFilePath);
+checkTextFile($secondFilePath);
 
-
-
+sub checkTextFile {
+	if ( -e $_[0] ) {
+		if ( -T $_[0] ) {
+			print "$_[0] is a TEXT file\n";
+		}
+		else {
+			print "$_[0] is NOT a TEXT file\n";
+			print "1. Proceed	2. Exit\n";
+			my $choice = <STDIN>;
+			chomp($choice);
+			if ( $choice == 2 ) {
+				exit;
+			}
+		}
+	}
+	else {
+		print "File Does Not Exist. Exiting Script";
+		exit;
+	}
+}
 
 print "------------- REPORT --------------- \n";
 
-my $originalPath = 'original.txt';
-if ( -T $originalPath ) {
-	print "$originalPath is a valid text file.\n";
-}
-else {
-	print "$originalPath is NOT valid text file.\n";
-}
-
-my $modifiedPath = 'modified.txt';
-if ( -T $modifiedPath ) {
-	print "$modifiedPath is a valid text file.\n";
-}
-else {
-	print "$modifiedPath is NOT valid text file.\n";
-}
-
-my $wc1 = `wc original.txt`;
+my $wc1 = `wc $firstFilePath`;
 my @splitwc1 = split( / /, $wc1 );
 print "\noriginal.txt META DATA:\n";
 print "Lines: $splitwc1[1]\n";
